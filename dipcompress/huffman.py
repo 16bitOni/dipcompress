e4f832c475ea8526326c5cd9319ca350acd0a032
16bitoni <subhadipmondal789@gmail.com>
2026-06-18 23:51:00 +0530

    Fix type in build_code_table function and implement huffman_encode and huffman_decode functions

--- dipcompress/huffman.py
+++ dipcompress/huffman.py
@@ -42,7 +42,7 @@ def build_huffman_tree(data: bytes) -> HuffNode:
         heapq.heappush(heap, merged)
     return heap[0]
 
-def build_code_table(root: HffNode) -> Dict[int, str]:
+def build_code_table(root: HuffNode) -> Dict[int, str]:
     codes = {}
 
     def walk(node: HuffNode, code: str):
@@ -58,6 +58,64 @@ def build_code_table(root: HffNode) -> Dict[int, str]:
     walk(root, '')
     return codes
 
+def huffman_encode(data: bytes) -> tuple[bytes,dict]:
+    if not data:
+        return b'', {}
+    
+    root = build_huffman_tree(data)
+    codes = build_code_table(root)
+
+    bit_string = ''.join(codes[byte] for byte in data)
+    
+    padding = (8-len(bit_string) % 8) % 8
+
+    bit_string += '0' * padding
+
+
+    encoded = bytearray()
+
+    for i in range(0, len(bit_string), 8):
+        byte_chunk = bit_string[i:i+8]
+        encoded.append(int(byte_chunk, 2))
+
+    metadata = {
+        'codes': codes,
+        'original_length': len(data),
+        'padding': padding
+    }
+
+
+    return bytes(encoded), metadata
+
+
+
+def huffman_decode(encoded:bytes, metadta: dict) -> bytes:
+    if not encoded:
+        return b""
+    
+    codes = metadta['codes']
+    original_length = metadta['original_length']
+    padding = metadta['padding']
+
+    bit_string = ''.join(f'{byte:08b}' for byte in encoded)
+
+    if padding:
+        bit_string = bit_string[:-padding]
+
+    reverse_codes = {v:k for k,v in codes.items()}
+
+    output = []
+    current = ''
+    for bit in bit_string:
+        current += bit
+        if current in reverse_codes:
+            output.append(reverse_codes[current])
+            current = ''
+
+        if len(output) == original_length:
+            break
+
+    return bytes(output)
 
 
 
